d128326db6fccfcc7661b91b2ca88853ad086431
Amalelz <amalsatheesh3002@gmail.com>
2025-10-04 23:09:57 +0530

    Login completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   );
